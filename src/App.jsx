07ce8dc638dcc7ca952fde49487b7502e0760175
nvonil <itslinonavarro@gmail.com>
2025-09-17 18:57:38 -0700

    Render Dashboard component in App with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Sidebar from "./components/sidebar/Sidebar";
+import Dashboard from "./components/pages/dashboard/Dashboard";
 
 import "./styles/App.css";
 
 function App() {
-    return <Sidebar />;
+    return (
+        <div className="app-container">
+            <Sidebar />
+            <div className="page-container">
+                <Dashboard />
+
+                <footer className="page-footer">&copy; 2025 LVN Fit</footer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
