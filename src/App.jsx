797dcf6a1e7443ecfeb369a63667b3f5a334604e
nvonil <itslinonavarro@gmail.com>
2025-10-03 20:51:24 -0700

    Add nutrition context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import WorkoutsProvider from "./components/contexts/WorkoutsContext";
+import NutritionProvider from "./components/contexts/NutritionContext";
 import Sidebar from "./components/sidebar/Sidebar";
 import Dashboard from "./components/pages/dashboard/Dashboard";
 import Workouts from "./components/pages/workouts/Workouts";
+import Nutrition from "./components/pages/nutrition/Nutrition";
 
 import "./styles/App.css";
 
@@ -11,15 +13,18 @@ function App() {
     return (
         <Router>
             <WorkoutsProvider>
-                <div className="app">
-                    <Sidebar />
-                    <main className="main">
-                        <Routes>
-                            <Route path="/" element={<Dashboard />} />
-                            <Route path="/workouts" element={<Workouts />} />
-                        </Routes>
-                    </main>
-                </div>
+                <NutritionProvider>
+                    <div className="app">
+                        <Sidebar />
+                        <main className="main">
+                            <Routes>
+                                <Route path="/" element={<Dashboard />} />
+                                <Route path="/workouts" element={<Workouts />} />
+                                <Route path="/nutrition" element={<Nutrition />} />
+                            </Routes>
+                        </main>
+                    </div>
+                </NutritionProvider>
             </WorkoutsProvider>
         </Router>
     );
